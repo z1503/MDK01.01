9aeb7708c896c1946e72ec5287ab8586ed2ef723
Zahar <zahar.z1503@gmail.com>
2024-02-19 12:41:12 +0300

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MDK01._01
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
